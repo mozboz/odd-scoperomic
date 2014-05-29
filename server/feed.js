0f0c2710444a6c72773fcb8e7743f1d4b5569461
mozboz <lewis.james@gmail.com>
2014-05-29 23:01:53 +0200

    changed to use json

--- server/feed.js
+++ server/feed.js
@@ -1,17 +1,27 @@
 
 var feedPollingHandle;
-var pollingInterval = 1000; // ms. How often to poll for changes on a remote API
+var pollingInterval = 2500; // ms. How often to poll for changes on a remote API
 var feedCheckTimeout = 10000; // ms. How long to wait for a response when validating a profile URL
 
 function startPolling(url, interval) {
     feedPollingHandle = Meteor.setInterval(
         function() {
-            allPosts = Meteor.http.call("GET", url);
-            posts = allPosts.content.split("\n");
-            for (i in posts) {
-                if (Posts.find({title: posts[i], url: url}).count() == 0) {
-                    Posts.insert({title: posts[i], url: url});
-                }
+            jsonData = Meteor.http.call("GET", url, {headers: {Accept: 'application/vnd.odd-profile.v1+json'}});
+            console.log('polled');
+            // console.log(jsonData.content);
+            var json = JSON.parse(jsonData.content);
+
+            for (var i in json.posts) {
+                console.log('json: ' + json.posts[i]);
+
+                var post = JSON.parse(json.posts[i]);
+                    console.log(post);
+
+                    console.log(post.title);
+                    console.log(post.url);
+                    if (Posts.find({title: post.title, url: post.url}).count() == 0) {
+                        Posts.insert({title: post.title, url: post.url});
+                    }
             }
         },
         interval);
@@ -23,7 +33,7 @@ function validateProfileUrl(url) {
     var Future = Npm.require("fibers/future");
     var fut = new Future();
     try {
-        Meteor.http.call("GET", url, {timeout: feedCheckTimeout}, function(error, result) {
+        Meteor.http.call("GET", url, {timeout: feedCheckTimeout, headers: {Accept: 'application/vnd.odd-profile.v1+json'}}, function(error, result) {
             if (result && result.statusCode == 200) {
                 fut.return(true);
             } else {
@@ -51,6 +61,7 @@ Meteor.methods({
 
     // This is where authentication step should be added
     updateUserProfileUrl: function (url) {
+        console.log("your user id is " + Meteor.userId());
         if (!validateProfileUrl(url)) {
             return "INVALID";
         } else {
