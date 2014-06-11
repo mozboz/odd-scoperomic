e69faa29f5818f3311277bf2dec1fb9ea95310bd
Daniel Janz <daniel@nbdj>
2014-06-12 01:29:30 +0200

    Added obj fields to autocomplete. Worked on composer.

--- client/views/objects/autocompleteEntry.js
+++ client/views/objects/autocompleteEntry.js
@@ -1,8 +1,31 @@
+Template.autocompleteEntry.rendered = function() {
+	jQuery(".autocomplete-entry-field").draggable({
+		helper:"clone",
+        appendTo: "body",
+        start: function(e, ui) {
+        	var oid = jQuery(jQuery(this).siblings("input").get(0)).val();
+        	jQuery(this).attr("data-oid", oid);
+//        	jQuery(this).attr("data-oid", oid);
+        }
+	});
+};
+
 Template.autocompleteEntry.events(
 {
 	'click .autocomplete-entry-expand' : function(e) {
-		var element = jQuery(e.target).parent();
 		
+		var element = jQuery(e.target).parent();		
+		var container = jQuery(".autocomplete-fields-container", element);
+
+		if (container.hasClass("collapsed")) {
+			container.removeClass("collapsed");
+			container.addClass("expanded");
+			container.height("auto");
+		} else {
+			container.removeClass("expanded");
+			container.addClass("collapsed");
+			container.height("18px");
+		}
 	},
 });
 
--- client/views/objects/composer.js
+++ client/views/objects/composer.js
@@ -1,3 +1,12 @@
+Template.composer.rendered = function() {
+	jQuery("#object-composer-container").droppable({
+		  drop: function( event, ui ) {
+			  var fromOid = jQuery(ui.helper).attr("data-oid");
+			  
+		  }
+	});
+};
+
 Template.composer.events(
 {
 	'keyup #search' : function(e) {
